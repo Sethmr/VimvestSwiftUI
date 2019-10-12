5a4af12509be7a8014bc1c6b59ab7103ea06d9ca
Seth Rininger <sethmr21@gmail.com>
2019-10-12 02:03:09 -0400

    Initial Commit

---
+++ VimvestSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VimvestSwiftUI
+//
+//  Created by Seth Rininger on 10/12/19.
+//  Copyright © 2019 Seth Rininger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

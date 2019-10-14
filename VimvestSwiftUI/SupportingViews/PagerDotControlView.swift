b2658884b3c19b7f9fb4f06e114c3ff0aa01bef8
Seth Rininger <seth@vimvest.com>
2019-10-14 11:58:09 -0400

    bleded dots

--- VimvestSwiftUI/SupportingViews/PagerDotControlView.swift
+++ VimvestSwiftUI/SupportingViews/PagerDotControlView.swift
@@ -15,11 +15,41 @@ struct PagerDotControlView: View {
     var body: some View {
         HStack(spacing: 5.clasp) {
             ForEach(0..<pagerInfo.pageCount, id: \.self) { index in
-                Rectangle()
-                    .fill(Color.default)
-                    .frame(diameter: self.diameter(at: index))
-                    .cornerRadius(self.cornerRadius(at: index))
+                ZStack {
+                    Rectangle()
+                        .fill(Color(white: 38 / 255).opacity(0.4))
+                        .frame(diameter: self.diameter(at: index))
+                        .cornerRadius(self.cornerRadius(at: index))
+                        .opacity(self.unselectedOpacity(at: index))
+                    Rectangle()
+                        .fill(Color.default)
+                        .frame(diameter: self.diameter(at: index))
+                        .cornerRadius(self.cornerRadius(at: index))
+                        .opacity(self.selectedOpacity(at: index))
+                }
             }
+        }
+    }
+
+    func unselectedOpacity(at index: Int) -> Double {
+        switch index {
+        case let x where x == pagerInfo.leftPage:
+            return (pagerInfo.pageOffset < 0 ? 1 - abs(Double(pagerInfo.pageOffset)) : Double(pagerInfo.pageOffset))
+        case let x where x == pagerInfo.leftPage + 1:
+            return (1 - Double(pagerInfo.pageOffset))
+        default:
+            return 1
+        }
+    }
+
+    func selectedOpacity(at index: Int) -> Double {
+        switch index {
+        case let x where x == pagerInfo.leftPage:
+            return (1 - Double(pagerInfo.pageOffset))
+        case let x where x == pagerInfo.leftPage + 1:
+            return (pagerInfo.pageOffset < 0 ? 1 - abs(Double(pagerInfo.pageOffset)) : Double(pagerInfo.pageOffset))
+        default:
+            return 0
         }
     }
 
